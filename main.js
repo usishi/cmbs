f691819c870951e53bdf322d13378a136c830d8d
Fatih Soydan <fatihsoydan@fatihsoydan.com>
2013-12-22 22:06:04 +0200

    bişeye benzesin yavaştan

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var jade = require('jade');
+var jade = require('jade'),
+	mime = require('mime'),
+	fs = require('fs');
 
 module.exports = function(ubsoptions) {
 
@@ -14,13 +16,22 @@ module.exports = function(ubsoptions) {
 
 	console.log('bune');
   return function(req, res, next) {
-  	switch (req.url){
-  		case '/newslist' :
-  			jade.renderFile(__dirname+'/jades/dene.jade',{ubsoptions:ubsoptions,pjson:{name:"projeismi"}},function (err, html) {
-					res.end(html);
-				});
-  		break;
-  		default : next(); break;
+  	if (req.url.indexOf('/files')>-1){
+  		console.log(req.url);
+  		var fname=__dirname+'/files/'+req.url.replace('/files/','');
+  		res.setHeader('Content-Type', mime.lookup(fname));
+  		
+  		//res.contentType(fname);
+  		res.end(fs.readFileSync(fname, 'utf8'));
+  	} else {
+  		switch (req.url){
+	  		case '/newslist' :
+	  			jade.renderFile(__dirname+'/jades/newslist.jade',{ubsoptions:ubsoptions,pjson:{name:"projeismi"}},function (err, html) {
+						res.end(html);
+					});
+	  		break;
+	  		default : next(); break;
+	  	}
   	}
   }
 };
